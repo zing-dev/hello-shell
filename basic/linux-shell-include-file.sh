0d959028e86c04059067a8c75fe3c4eae75e62d4
zhangrxiang <zhangrxiang@yahoo.com>
2017-01-14 23:32:13 +0800

    呵呵

---
+++ basic/linux-shell-include-file.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+zing="zhangrongxianglikehoulingyan"
---
+++ basic/linux-shell-include-file2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+. linux-shell-include-file.sh
+. ./linux-shell-include-file.sh
+source ./linux-shell-include-file.sh
+echo $zing
+
